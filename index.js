8f0d8129570ace1dbd2c80d4d984246b90c8f6ef
Bekaidin <janarbekaidin@gmail.com>
2023-03-12 14:56:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/openai',require('./routes/openaiRoutes'));
+
+app.listen(port,()=>console.log(`Server started on port ${port}`));
